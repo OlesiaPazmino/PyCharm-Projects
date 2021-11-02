f1e618dafa2d888c2a20d106e797c45c356a9362
OlesiaPazmino <88597685+OlesiaPazmino@users.noreply.github.com>
2021-11-02 13:45:19 -0500

    initial commit

---
+++ lesson1.py
@@ -0,0 +1,9 @@
+#print("Hello my dear friend")
+#print_this_line="Hello my dear friend,Alex"
+#print(print_this_line)
+print("______")
+print("|"" "" " " " "/")
+print("|"" " " " "/")
+print("|" " " "/")
+print("|/")
+
