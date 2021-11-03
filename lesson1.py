eed2c94b8e6ccccb03ae8788cbc0bd4322101dd3
OlesiaPazmino <88597685+OlesiaPazmino@users.noreply.github.com>
2021-11-03 13:42:43 -0500

    update

--- lesson1.py
+++ lesson1.py
@@ -1,9 +1,19 @@
 #print("Hello my dear friend")
 #print_this_line="Hello my dear friend,Alex"
 #print(print_this_line)
-print("______")
-print("|"" "" " " " "/")
-print("|"" " " " "/")
-print("|" " " "/")
-print("|/")
+#print("______")
+#print("|"" "" " " " "/")
+#print("|"" " " " "/")
+#print("|" " " "/")
+#print("|/")
+my_string = "Test Automation"
+word_test = slice(4)
+print(my_string[word_test])
+my_string = "Test Automation"
+second_word =my_string [-10:]
+print(second_word)
+
+my_list = [10,50,20,25,30]
+sliced_list= my_list [2:4]
+print(sliced_list)
 
