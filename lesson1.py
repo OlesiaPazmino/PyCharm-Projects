71a2819571c1c02cf8acd69a6a260dea3f08cc5e
OlesiaPazmino <88597685+OlesiaPazmino@users.noreply.github.com>
2021-11-04 14:25:41 -0500

    update

--- lesson1.py
+++ lesson1.py
@@ -6,14 +6,30 @@
 #print("|"" " " " "/")
 #print("|" " " "/")
 #print("|/")
-my_string = "Test Automation"
-word_test = slice(4)
-print(my_string[word_test])
-my_string = "Test Automation"
-second_word =my_string [-10:]
-print(second_word)
+#my_string = "Test Automation"
+#word_test = slice(4)
+#print(my_string[word_test])
+#my_string = "Test Automation"
+#second_word =my_string [-10:]
+#print(second_word)
 
-my_list = [10,50,20,25,30]
-sliced_list= my_list [1:3]
-print(sliced_list)
+#my_list = [10,50,20,25,30]
+#sliced_list= my_list [1:3]
+#print(sliced_list)
+#num =1;
+def list_benefits():
+    benefits_list = ["More organized code", "More readable code", "Easier code reuse", "Allowing programmers to share and connect code together"]
+    return benefits_list
+    pass
+list_benefits()
+def build_sentence(info):
+    return info + " is a benefit of functions!"
+    pass
+def name_the_benefits_of_functions():
+    list_of_benefits = list_benefits()
+    print(build_sentence(list_of_benefits[0]))
+    print(build_sentence(list_of_benefits[1]))
+    print(build_sentence(list_of_benefits[2]))
+    print(build_sentence(list_of_benefits[3]))
+name_the_benefits_of_functions()
 
